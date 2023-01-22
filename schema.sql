6930bfb2c26efaa7b9150f6d965d0bd56ad058ee
chancydarius <111719953+chancydarius@users.noreply.github.com>
2023-01-21 23:59:49 -0500

    Created Schema

---
+++ schema.sql
@@ -0,0 +1,31 @@
+-- Creating tables for Wine_QualiDB
+
+-- DROP TABLE Red_Wine;
+CREATE TABLE Red_Wine (
+    "fixed acidity" float NOT NULL,
+    "volatile acidity" float NOT NULL,
+    "citric acid" float NOT NULL,
+    "residual sugar" float NOT NULL,
+    "chlorides" float NOT NULL,
+    "free sulfur dioxide" float NOT NULL,
+    "total sulfur dioxide" float NOT NULL,
+    "density" float NOT NULL,
+    "pH" float NOT NULL,
+    "sulphates" float NOT NULL,
+    "alcohol" float NOT NULL
+);
+
+-- DROP TABLE White_Wine;
+CREATE TABLE White_Wine (
+    "fixed acidity" float NOT NULL,
+    "volatile acidity" float NOT NULL,
+    "citric acid" float NOT NULL,
+    "residual sugar" float NOT NULL,
+    "chlorides" float NOT NULL,
+    "free sulfur dioxide" float NOT NULL,
+    "total sulfur dioxide" float NOT NULL,
+    "density" float NOT NULL,
+    "pH" float NOT NULL,
+    "sulphates" float NOT NULL,
+    "alcohol" float NOT NULL
+);
