62c2a5538685f5d4d93a423cccd6e884deaca325
chancydarius <111719953+chancydarius@users.noreply.github.com>
2023-01-22 00:00:20 -0500

    Created Sample Query

---
+++ queries.sql
@@ -0,0 +1,20 @@
+SELECT * FROM Red_Wine;
+SELECT * FROM White_Wine;
+
+SELECT ___
+FROM ___
+WHERE ______ BETWEEN ______ AND ______;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+-- DROP TABLE ______;
+SELECT 
+______
+______
+______
+INTO ______
+FROM ______ AS ___
+INNER JOIN ______ AS ___ 
+ON (______ = ______)
+WHERE (______ BETWEEN ______ AND ______)
+ORDER BY ______;
+SELECT * FROM _______;
